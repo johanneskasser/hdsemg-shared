7de2cc306e7282ddf001c17ee5064c28dd336ef2
Johannes Kasser <johanneskasser@outlook.de>
2025-04-15 10:57:48 +0200

    Add pyproject.toml and update release workflow for package publishing

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #src/shared_logic/setup.py
+import os
+
 from setuptools import setup, find_packages
+
+version = os.getenv("PACKAGE_VERSION", "0.0.1")
 
 setup(
     name='hdsemg-shared',
-    version='0.0.1',
+    version=version,
     packages=find_packages(),
     install_requires=[
         "numpy",
