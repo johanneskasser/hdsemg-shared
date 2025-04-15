a4eba41bfd46062b3a5f17638b389fd5ba9de16c
Johannes Kasser <johanneskasser@outlook.de>
2025-04-15 16:06:49 +0200

    Add public API functions for grid and file operations in __init__.py

---
+++ __init__.py
@@ -0,0 +1,5 @@
+from .logic.grid import extract_grid_info, load_single_grid_file
+from .logic.fileio.file_io import load_file
+from .logic.fileio.matlab_file_io import save_selection_to_mat, save_selection_to_json
+
+__all__ = ["load_file", "save_selection_to_mat", "save_selection_to_json", "extract_grid_info", "load_single_grid_file"]
