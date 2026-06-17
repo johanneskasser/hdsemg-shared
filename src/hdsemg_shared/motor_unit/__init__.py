0e1ef4f091630d9b330a13a9f03c22f05ec0bbfe
Harald Penasso <harald.penasso@gmail.com>
2026-06-17 23:46:48 +0200

    Update __init__.py
    brace_pic.py import

--- src/hdsemg_shared/motor_unit/__init__.py
+++ src/hdsemg_shared/motor_unit/__init__.py
@@ -1,11 +1,22 @@
 """
-Motor-unit discharge analysis for HD-sEMG.
+Motor-unit discharge and PIC brace-method analysis for HD-sEMG.
 
-Currently provides the brace-height method of Beauchamp et al. (2023) for
-estimating persistent inward current (PIC) amplification from a single
-motor-unit (MU) discharge profile, together with the discharge-rate utilities it
-builds upon.
+This subpackage provides utilities for converting decomposed motor-unit (MU)
+spike trains into instantaneous and smoothed discharge-rate traces, together
+with a brace-method implementation for estimating PIC-related discharge
+nonlinearity from a single MU.
+
+The preferred brace-method API is the module namespace:
+
+>>> from hdsemg_shared.motor_unit import brace_pic
+>>> result = brace_pic.compute_brace_pic(smooth_rate_pps, reference_percent_mvt)
 """
+
+# Module namespace exports:
+#     from hdsemg_shared.motor_unit import discharge_rate
+#     from hdsemg_shared.motor_unit import brace_pic
+from . import discharge_rate
+from . import brace_pic
 
 from .discharge_rate import (
     MIN_DISCHARGES,
@@ -14,21 +25,43 @@ from .discharge_rate import (
     instantaneous_discharge_rate,
     smooth_discharge_rate_svr,
 )
-from .brace_height import (
+
+from .brace_pic import (
     MAX_BRACE_HEIGHT_NORM,
-    BraceHeightResult,
-    brace_height_from_spike_train,
-    compute_brace_height,
+    MetricInterval,
+    BracePICCI,
+    CIOptions,
+    BracePICResult,
+    compute_brace_pic,
+    pics_brace,
+    compute_pic_brace,
+    brace_pic_from_spike_train,
+    pics_brace_openhdemg_all,
+    plot_brace,
 )
 
 __all__ = [
+    # submodule namespaces
+    "discharge_rate",
+    "brace_pic",
+
+    # discharge-rate utilities
     "MIN_DISCHARGES",
     "firing_times_from_binary",
     "firing_times_from_indices",
     "instantaneous_discharge_rate",
     "smooth_discharge_rate_svr",
+
+    # brace-method PIC exports
     "MAX_BRACE_HEIGHT_NORM",
-    "BraceHeightResult",
-    "brace_height_from_spike_train",
-    "compute_brace_height",
+    "MetricInterval",
+    "BracePICCI",
+    "CIOptions",
+    "BracePICResult",
+    "compute_brace_pic",
+    "pics_brace",
+    "compute_pic_brace",
+    "brace_pic_from_spike_train",
+    "pics_brace_openhdemg_all",
+    "plot_brace",
 ]
